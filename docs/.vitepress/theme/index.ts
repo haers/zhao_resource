68e7d20f7a3933b30e1915ba566d56e5ea784eb2
nihao <jingtingchangqing@gmail.com>
2026-01-17 22:22:11 +0800

    giscus配置

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -116,10 +116,10 @@ export default {
 
     // giscus配置
     giscusTalk({
-      repo: 'Yiov/vitepress-doc', //仓库
-      repoId: 'R_kgDOGYFl1A', //仓库ID
+      repo: 'haers/zhao_resource', //仓库
+      repoId: 'R_kgDOOTK3lw', //仓库ID
       category: 'General', // 讨论分类
-      categoryId: 'DIC_kwDOGYFl1M4CayLN', //讨论分类ID
+      categoryId: 'DIC_kwDOOTK3l84C1Ext', //讨论分类ID
       mapping: 'pathname',
       inputPosition: 'bottom',
       lang: 'zh-CN',
